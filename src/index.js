f956e958cb97a827174c71b063abe1097f5e08ba
Valdesrep <90558354+Valdesrep@users.noreply.github.com>
2022-03-20 19:47:09 +0200

    добавляет слушатели событий

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import './css/styles.css';
+import { fetchImage } from './js/fetchImage';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-const axios = require('axios');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
-    gallery: document.querySelector(".gallery"),
-    form: document.querySelector(".search-form")
+  gallery: document.querySelector('.gallery'),
+  form: document.querySelector('.search-form'),
+  btn: document.querySelector('button[type="submit"] '),
+  input: document.querySelector('[name="searchQuery"]'),
 };
 
+let page;
+let imageToFind;
+
+refs.btn.addEventListener('click', onBtnClick);
+refs.form.addEventListener('input', onFormInput);
+
+function onBtnClick(event) {
+  page = 1;
+  event.preventDefault();
+}
+
+function onFormInput() {
+  imageToFind = refs.input.value.trim();
+  return imageToFind;
+}
+
+const lightbox = new SimpleLightbox('.gallary a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function getMurkup(img) {
+  const murkup = photoTmp(img);
+  refs.gallery.insertAdjacentHTML('beforeend', murkup);
+  lightbox.refresh();
+}
--- src/js/fetchImage.js
+++ src/js/fetchImage.js
@@ -2,6 +2,11 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '26220037-ba2b9defb736764e21d2f2b28';
 const axios = require('axios').default;
 
-async function fetchImage() {
+async function fetchImage(img, page) {
   const params = `image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+  const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${img}&${params}`);
+  const images = response.data;
+  return images;
 }
+
+export { fetchImage };
